b7cc111231d3dba8643777461307727385581b20
SUPER_MEN4iG <supermen4ig988@gmail.com>
2018-01-12 02:45:25 +0400

    verstka is up

--- test-app.web/src/app/app.component.ts
+++ test-app.web/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 import { AuthenticationService } from './_services/authentication.service';
 
 @Component({
-  selector: 'app-root',
+  selector: 'body',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
