a031f53d38051a6dd8b01adb2209693f19bca189
SUPER_MEN4iG <supermen4ig988@gmail.com>
2018-01-18 17:26:51 +0400

    profile init

--- test_app.api/test_app.api/Migrations/20180118122227_v15.Designer.cs
+++ test_app.api/test_app.api/Migrations/20180118122227_v15.Designer.cs
@@ -11,9 +11,10 @@ using test_app.api.Data;
 namespace test_app.api.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180118122227_v15")]
+    partial class v15
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ test_app.api/test_app.api/Migrations/20180118122227_v15.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace test_app.api.Migrations
+{
+    public partial class v15 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rarity",
+                table: "StackCases");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Rarity",
+                table: "Skins",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rarity",
+                table: "Skins");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Rarity",
+                table: "StackCases",
+                nullable: true);
+        }
+    }
+}
