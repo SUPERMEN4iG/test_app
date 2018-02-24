3228f020fcaa9ca095bcca99b55eb9ca0d291f26
Alexart30region <alexart30region13@gmail.com>
2018-02-25 02:07:08 +0400

    fix

--- test_app.api/test_app.api/Extensions/IEnumerableExtensions.cs
+++ test_app.api/test_app.api/Extensions/IEnumerableExtensions.cs
@@ -6,23 +6,31 @@ using System.Threading.Tasks;
 namespace test_app.api.Extensions
 {
     public static class IEnumerableExtensions
+
+
     {
+        private static Random _random = new Random();
         public static T RandomElementByWeight<T>(this IEnumerable<T> sequence, Func<T, float> weightSelector)
         {
             float totalWeight = sequence.Sum(weightSelector);
-            double itemWeightIndex = new Random().NextDouble() * totalWeight;
-            float currentWeightIndex = 0;
+            double itemWeightIndex = GetRandomNumber(1, totalWeight);
 
             foreach (var item in sequence.Select(x => new { Value = x, Weight = weightSelector(x) }))
             {
-                currentWeightIndex -= item.Weight;
+                itemWeightIndex -= item.Weight;
 
-                if (currentWeightIndex <= 0)
+                if (itemWeightIndex <= 0)
                     return item.Value;
             }
 
             return default(T);
 
+        }
+
+        private static double GetRandomNumber(double minimum, double maximum)
+        {
+
+            return _random.NextDouble() * (maximum - minimum) + minimum;
         }
     }
 }
